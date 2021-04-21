ad23bc186d0998aef007e8f4e531038b13244417
SavStanis <42719022+SavStanis@users.noreply.github.com>
2021-04-22 00:09:20 +0300

    Add lab5 Create floder for lab3

--- lab3/src/test/java/com/savstanis/softwaretesting/lab3/PrivatePolicyTest.java
+++ lab3/src/test/java/com/savstanis/softwaretesting/lab3/PrivatePolicyTest.java
@@ -7,8 +7,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
---
+++ lab5/src/test/java/org/savstanis/lab5/MainTests.java
@@ -0,0 +1,62 @@
+package org.savstanis.lab5;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.savstanis.lab5.model.User;
+
+
+public class MainTests {
+
+    @Test
+    public void findUsers_bodyShouldBePresent() {
+        new UserEndpoint()
+                .getUsers()
+                .then()
+                .assertThat()
+                .body(Matchers.notNullValue());
+    }
+
+    @Test
+    public void findUserById_bodyShouldBePresent() {
+        new UserEndpoint()
+                .getUserById("123")
+                .then()
+                .assertThat()
+                .body(Matchers.notNullValue());
+    }
+
+    @Test
+    public void createUser_userShouldBeCreated() {
+        User user = new User("Name", "email@mail.com", "Male", "Active");
+
+        new UserEndpoint()
+                .createUser(user, System.getenv("ACCESS_TOKEN"))
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void updateUser_userShouldBeUpdated() {
+        User user = new User("Name", "email2@mail.com", "Female", "Active");
+
+        new UserEndpoint()
+                .updateUser("1666", user, System.getenv("ACCESS_TOKEN"))
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void deleteUser_authFailed() {
+        User user = new User("Name", "email@mail.com", "Male", "Active");
+
+        Response baseResponse = new UserEndpoint().deleteUser("1666", "");
+
+        JsonPath jsonPath = baseResponse.jsonPath();
+        Assert.assertEquals( 401, jsonPath.getInt("code"));
+    }
+}
